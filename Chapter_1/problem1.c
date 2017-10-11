22ad2e878d970108664039f411bdc0b82b5e6018
Alex <adminser@ncsu.edu>
2017-10-11 12:04:59 -0400

    Problem 1 solved

--- Chapter_1/problem1.c
+++ Chapter_1/problem1.c
@@ -4,15 +4,21 @@
 
 
 
-
+#include<stdio.h>
+#include<time.h>
+#include<limits.h>
 
 
 
 
 
 int main() {
+    time_t currTime = time(NULL);
+    time_t maxTime = INT_MAX;
+    double diffTime = difftime(maxTime, currTime);
+    int years = (int) diffTime / (60*60*24*365);
+    printf("%d\n", years);
 
 
-
-  return 0;
+    return 0;
 }
